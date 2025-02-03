ca8734766ac313af8b698185862bec3c9450b005
ryancleary <ryancleary@localhost>
2025-02-03 11:40:27 -0500

    Added println

--- src/eclipseGitDemoW25/HelloWorld.java
+++ src/eclipseGitDemoW25/HelloWorld.java
@@ -1,10 +1,6 @@
 package eclipseGitDemoW25;
-
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
 	}
-
 }
