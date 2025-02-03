125f6758cc02e6d03e8971246235c3147a46689f
ryancleary <ryancleary@localhost>
2025-02-03 11:49:29 -0500

    Programmer 2 changes

--- src/eclipseGitDemoW25/HelloWorld.java
+++ src/eclipseGitDemoW25/HelloWorld.java
@@ -2,5 +2,6 @@ package eclipseGitDemoW25;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Programmer 2 did this");
 	}
 }
